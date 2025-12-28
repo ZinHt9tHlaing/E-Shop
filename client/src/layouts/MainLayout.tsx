fedd0c29507922f72b406cf648feb7ff27eda4bd
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-12-28 11:19:43 +0630

    added page transition with framer-motion

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,14 +1,32 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
+import { AnimatePresence, motion } from "framer-motion";
+
+const pageVariants = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  // exit: { opacity: 0, y: -20, transition: { duration: 0.3 } },
+};
 
 const MainLayout = () => {
+  const location = useLocation();
+
   return (
     <section>
       <Navbar />
-      <main className="max-w-6xl mx-auto py-6 px-4 mt-32">
-        <Outlet />
-      </main>
+      <AnimatePresence>
+        <motion.main
+          key={location.pathname}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={pageVariants}
+          className="max-w-6xl mx-auto py-6 px-4 mt-32"
+        >
+          <Outlet />
+        </motion.main>
+      </AnimatePresence>
       <Footer />
     </section>
   );
