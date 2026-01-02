faec00b14f2591b57666e1bb9fa85b6b3ba96877
ZinHt9tHlaing <phyoearkar267@gmail.com>
2026-01-02 22:26:06 +0630

    table pagination

--- client/src/components/products/ProductTable.tsx
+++ client/src/components/products/ProductTable.tsx
@@ -3,6 +3,7 @@ import useProductColumns from "./ProductTableColumns";
 import {
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -14,18 +15,39 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 interface ProductTableProps {
   data: Product[];
 }
 
 const ProductTable = ({ data }: ProductTableProps) => {
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+
   const columns = useProductColumns();
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      pagination,
+    },
+    onPaginationChange: setPagination,
   });
 
   return (
@@ -72,16 +94,64 @@ const ProductTable = ({ data }: ProductTableProps) => {
               ))
             ) : (
               <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="text-center"
-                >
-                  No results.
+                <TableCell colSpan={columns.length} className="text-center">
+                  No products.
                 </TableCell>
               </TableRow>
             )}
           </TableBody>
         </Table>
+
+        {/* pagination */}
+        <div className="flex items-center gap-4 justify-end mt-4 text-xs">
+          <Select
+            value={String(table.getState().pagination.pageSize)}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value));
+            }}
+          >
+            <SelectTrigger className="w-24 h-5 text-xs">
+              <SelectValue placeholder="Rows" />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Rows per page</SelectLabel>
+                {[5, 10, 20, 50].map((pageSize) => (
+                  <SelectItem key={pageSize} value={String(pageSize)}>
+                    Show {pageSize}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <Button
+            size={"sm"}
+            variant={"outline"}
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+            className="cursor-pointer active:scale-90 duration-150"
+          >
+            Prev
+          </Button>
+
+          {/* page starts 1 */}
+          <span className="text-xs font-medium">
+            Page {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </span>
+
+          <Button
+            size={"sm"}
+            variant={"outline"}
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+            className="cursor-pointer active:scale-90 duration-150"
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
